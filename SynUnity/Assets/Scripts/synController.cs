5861d36e735cb33ee24636a3948bcdeb25b94cfe
Casidar <casidar5@gmail.com>
2021-07-22 16:01:34 -0700

    Simplifying the character controller

--- SynUnity/Assets/Scripts/synController.cs
+++ SynUnity/Assets/Scripts/synController.cs
@@ -47,160 +47,42 @@ public class synController : MonoBehaviour
     SpriteRenderer sprRen;
     void Start()
     {
-        rigbod = GetComponent<Rigidbody2D>();
-        respawnPosition = transform.position;
-        bulletTimer = timeBetweenShot;
-        direction = 2;
-        health = maxHealth;
-        sprRen = GetComponent<SpriteRenderer>();
+        rigbod = GetComponent<Rigidbody2D>(); //Getting the rigidbody component
+
+        respawnPosition = transform.position; //Setting the respawn position to the starting location
+        bulletTimer = timeBetweenShot; //Setting the bullet timer to be ready for firing
+        direction = -2; // Setting the bullet direction to the default direction
+        health = maxHealth; //Setting the health to the max health
+        sprRen = GetComponent<SpriteRenderer>(); // Getting the comonent of the Sprite renderer
     }
     
     void Update()
     {
-        velocity = rigbod.velocity;
-        horiMove = Input.GetAxisRaw("Horizontal");
+        velocity = rigbod.velocity; //Setting the velocity variable to the rigidbody velocity
+        horiMove = Input.GetAxisRaw("Horizontal"); //Horizontal Axis
+
         if(health > 0){
-            if(smoothAirMovement){
-                if(rigbod.velocity.y == 0){
-                    if((Input.GetKey("s") || Input.GetKey("down")) && sliding){ //sliding
-                        velocity.x = Mathf.Lerp(velocity.x, 0, Time.deltaTime/slideDecel);
-                    }
-                    else{
-                        velocity.x = horiMove * speed;
-                    }
-                    
-                }
-                else{
-                    velocity.x += horiMove * smoothAirMovementSpeed * Time.deltaTime;
-                    if(velocity.x > speed){
-                        velocity.x = speed;
-                    }
-                    else if(velocity.x < -speed){
-                        velocity.x = -speed;
-                    }
-                }
-            }
-            else{
-                velocity.x = horiMove * speed;
-            }
-            
-
-            if(Input.GetKeyDown("w") || Input.GetKeyDown("up")){
-                if(rigbod.velocity.y == 0){
-                    velocity.y = jumpheight;
-                    jump.Play();
-                } 
-                else if(jumpBool){
-                    jumpBool = false;
-                    velocity.y = doubleJumpHeight;
-                    jump.Play();
-                }
-                
-            }
-
-            if(Input.GetKey("w") || Input.GetKey("up")){
-                if(rigbod.velocity.y != 0 && jetPack && jetPackFuel > 0){
-                    rigbod.AddForce(new Vector2(0,jetPackForce*Time.deltaTime));
-                    jetPackFuel -= Time.deltaTime;
-                }
-            }
-
-            if(jetPackFuel < 0){
-                jetPackFuel = 0;
-            }
+            xMovement();
+            JumpControl();
+            JetPackControl();
 
             if(rigbod.velocity.y == 0 && doubleJump){
                 jumpBool = true;
             }
 
-            if(Input.GetKeyDown("w") || Input.GetKeyDown("up")){
-                direction = 0;
-            }
-            if((Input.GetKey("right") || Input.GetKey("d")) && (Input.GetKey("left") || Input.GetKey("a"))){}
-            else if(Input.GetKey("right") || Input.GetKey("d")){
-                if(Input.GetKey("w") || Input.GetKey("up")){
-                    direction = 1;
-                }
-                else{
-                    direction = 2;
-                }
-            }
-            else if(Input.GetKey("left") || Input.GetKey("a")){
-                if(Input.GetKey("w") || Input.GetKey("up")){
-                    direction = -1;
-                }
-                else{
-                    direction = -2;
-                }
-            }
+            CheckFireDirection();
             
-            if(bulletTimer < timeBetweenShot){
-                bulletTimer += Time.deltaTime;
-            }
-            else if(Input.GetKeyDown(fireKey)){
-                if(direction == 2)
-                    spawnBullet(0);
-                else if(direction == 1)
-                    spawnBullet(45);
-                else if(direction == 0)
-                    spawnBullet(90);
-                else if(direction == -1)
-                    spawnBullet(135);
-                else if(direction == -2)
-                    spawnBullet(180);
-                bulletTimer = 0;
-            }
-
-            if(velocity.x > 0.001f){
-                GetComponent<SpriteRenderer>().flipX = true;
-            }
-            else if(velocity.x < -0.001f){
-                GetComponent<SpriteRenderer>().flipX = false;
-            }
-
-            if(Input.GetKey(runKey) && !Input.GetKey("s")){
-                velocity.x *= runMultiplier;
-            }
+            CheckBulletFire();
         }
         else{
-            if(respawnTimer == 0){
-                ded.Play();
-            }
-            respawnTimer += Time.deltaTime;
-            if(respawnTimer >= respawnTimeLimit){
-                health = maxHealth;
-                respawnTimer = 0;
-                transform.position = respawnPosition;
-            }
-            velocity.x = 0;
+            DeathAction();
         }
 
         respawnCheck();
+        AnimationControl();
+        UpdateHealthText();
 
-        if(velocity.y == 0){
-            //sprRen.sprite = idle;
-        }
-        if(velocity.y != 0){
-            onGround = false;
-            //sprRen.sprite = inAir;
-        }
-
-        animatorController.SetFloat("yVelocity", velocity.y);
-        if(velocity.x > 0 || velocity.x < 0){
-            animatorController.SetBool("Walking", true);
-        }
-        else{
-            animatorController.SetBool("Walking", false);
-        }
-        
-
-
-        if(health < 0){
-            health = 0;
-        }
-        healthText.GetComponent<UnityEngine.UI.Text>().text = "Health: " + health;
-
-        rigbod.velocity = velocity;
+        rigbod.velocity = velocity; //Set velocity to 
     }
 
     void respawnCheck(){
@@ -213,10 +95,120 @@ public class synController : MonoBehaviour
             velocity = new Vector2(0,0);
         }
     }
+    void AnimationControl(){
+        if(velocity.x > 0.001f){
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if(velocity.x < -0.001f){
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
 
+        animatorController.SetFloat("yVelocity", velocity.y);
+        if(velocity.x > 0 || velocity.x < 0){
+            animatorController.SetBool("Walking", true);
+        }
+        else{
+            animatorController.SetBool("Walking", false);
+        }
+    }
+    void xMovement(){
+        if(smoothAirMovement){
+            if(rigbod.velocity.y == 0){
+                if((Input.GetKey("s") || Input.GetKey("down")) && sliding){ //sliding
+                    velocity.x = Mathf.Lerp(velocity.x, 0, Time.deltaTime/slideDecel);
+                }
+                else{
+                    velocity.x = horiMove * speed;
+                }
+                
+            }
+            else{
+                velocity.x += horiMove * smoothAirMovementSpeed * Time.deltaTime;
+                if(velocity.x > speed){
+                    velocity.x = speed;
+                }
+                else if(velocity.x < -speed){
+                    velocity.x = -speed;
+                }
+            }
+        }
+        else{
+            velocity.x = horiMove * speed;
+        }
+        if(Input.GetKey(runKey) && !Input.GetKey("s")){
+            velocity.x *= runMultiplier;
+        }
+    }
+    void JumpControl(){
+        if(Input.GetKeyDown("w") || Input.GetKeyDown("up")){
+            if(rigbod.velocity.y == 0){
+                velocity.y = jumpheight;
+                jump.Play();
+            } 
+            else if(jumpBool){
+                jumpBool = false;
+                velocity.y = doubleJumpHeight;
+                jump.Play();
+            }
+        }
+        if(rigbod.velocity.y == 0 && doubleJump){ //Double jump reset
+            jumpBool = true;
+        }
+    }
+    void JetPackControl(){
+        if(Input.GetKey("w") || Input.GetKey("up")){
+            if(rigbod.velocity.y != 0 && jetPack && jetPackFuel > 0){
+                rigbod.AddForce(new Vector2(0,jetPackForce*Time.deltaTime));
+                jetPackFuel -= Time.deltaTime;
+            }
+        }
+
+        if(jetPackFuel < 0){
+            jetPackFuel = 0;
+        }
+    }
+    void UpdateHealthText(){
+        if(health < 0){
+            health = 0;
+        }
+        healthText.GetComponent<UnityEngine.UI.Text>().text = "Health: " + health;
+    }
+    void CheckFireDirection(){
+
+    }
+    void CheckBulletFire(){
+        if(bulletTimer < timeBetweenShot){
+            bulletTimer += Time.deltaTime;
+        }
+        else if(Input.GetKeyDown(fireKey)){
+            if(direction == 2)
+                spawnBullet(0);
+            else if(direction == 1)
+                spawnBullet(45);
+            else if(direction == 0)
+                spawnBullet(90);
+            else if(direction == -1)
+                spawnBullet(135);
+            else if(direction == -2)
+                spawnBullet(180);
+            bulletTimer = 0;
+        }
+    }
     void spawnBullet(float rotation){
         Instantiate(bulletPrefab, transform.position, Quaternion.Euler(0,0,rotation));
         pew.Play();
+    }
+    void DeathAction(){
+        if(respawnTimer == 0){
+            ded.Play();
+        }
+        respawnTimer += Time.deltaTime;
+        if(respawnTimer >= respawnTimeLimit){
+            health = maxHealth;
+            respawnTimer = 0;
+            transform.position = respawnPosition;
+        }
+        velocity.x = 0;
     }
 
     void OnTriggerEnter2D(Collider2D other){
